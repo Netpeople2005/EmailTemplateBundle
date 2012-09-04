d5e8c593703e74f2ddc15064ef462253a90c18fd
Manuel Aguirre <programador.manuel@gmail.com>
2012-09-04 17:45:59 -0430

    creando una vista para visualizar los templates que se pretenden enviar

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -4,12 +4,28 @@ namespace Netpeople\EmailTemplateBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-
 class DefaultController extends Controller
 {
-    
-    public function indexAction($name)
+
+    public function indexAction()
     {
-        return $this->render('EmailTemplateBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('EmailTemplateBundle:Default:index.html.twig');
     }
+
+    public function renderTemplateAction()
+    {
+        $data = json_decode($this->getRequest()->get('data'), TRUE);
+        $view = $this->getRequest()->get('view');
+        $locale = $this->getRequest()->get('locale');
+
+        $currentLocale = $this->get('translator')->getLocale();
+        $this->get('translator')->setLocale($locale);
+
+        $response = $this->render($view, array_merge($data, array($locale)));
+
+        $this->get('translator')->setLocale($currentLocale);
+
+        return $response;
+    }
+
 }
