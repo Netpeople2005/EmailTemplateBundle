322a025a37a75130db145d954c9d75d1accd6d66
Manuel Aguirre <programador.manuel@gmail.com>
2012-09-07 16:38:19 -0430

    agregada una lib para editar json
    Con esta lib podemos configurar y mantener guardados los json
    que contienen los parametros que se envian al template para las pruebas

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Netpeople\EmailTemplateBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Netpeople\EmailTemplateBundle\Locator\FilesBundle;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -22,6 +23,8 @@ class DefaultController extends Controller
         $data = (array) json_decode($this->getRequest()->get('data'), TRUE);
         $view = $this->getRequest()->get('view');
         $locale = $this->getRequest()->get('locale');
+
+        $this->saveData($view, $data);
 
         $currentLocale = $this->get('translator')->getLocale();
         $this->get('translator')->setLocale($locale);
@@ -45,11 +48,36 @@ class DefaultController extends Controller
         foreach ($files as $view) {
             $views[] = str_replace('/', ':', substr($view, strpos($view, '/Resources/views/') + 17));
         }
-        
+
         return $this->render('EmailTemplateBundle:Default:view_files.html.twig', array(
                     'files' => $views
                 ));
+    }
 
+    private function saveData($view, $data)
+    {
+        $dir = dirname(__DIR__) . '/Resources/json/';
+        if (!is_dir($dir)) {
+            if (!is_writable($dir)) {
+                return;
+            }
+            @mkdir($dir, 0777, true);
+        }
+        file_put_contents($dir . md5($view) . '.json', json_encode($data));
+    }
+
+    public function loadDataAction($view)
+    {
+        $file = dirname(__DIR__) . '/Resources/json/' . md5($view) . '.json';
+        //var_dump($file);die;
+        if (!file_exists($file)) {
+            $json = '{}';
+        } else {
+            $json = file_get_contents($file);
+        }
+        return new Response($json, 200, array(
+                    'Content-Type' => "application/json"
+                ));
     }
 
 }
